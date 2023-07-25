eadee97ea4092dc11eeb6bef928d6dce7f21d40f
eldray <83376852+eldray@users.noreply.github.com>
2023-07-25 12:52:52 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 """Script to export data in the CSV format"""
-import csv
 import requests as r
+import csv
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
-    to_do = r.get(url + "todos", params={"userId": user_id}).json()
+def export_to_csv(employee_id, todos_data):
+    employee_name = todos_data[0]['name']
+    file_name = f"{employee_id}.csv"
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, elm.get("completed"),
-<<<<<<< HEAD
-                          elm.get("title")]) for elm in to_do]
+    with open(file_name, mode='w', newline='') as csv_file:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
 
-=======
-                          elm.get("title")]) for elm in to_do]
->>>>>>> d6a8d0dcf23b28829cd6b9cf5fd4f6415a4cc277
+        for todo in todos_data:
+            writer.writerow({
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
+                'TASK_COMPLETED_STATUS': str(todo['completed']),
+                'TASK_TITLE': todo['title']
+            })
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee_id = sys.argv[1]
+    usr_data = r.get(url + 'users/{}'.format(employee_id)).json()
+    to_do = r.get(url + 'todos', params={'userId': employee_id}).json()
+
+    export_to_csv(employee_id, to_do)
