2ceaa2b46371c65e7234c1036d870299523c0669
eldray <83376852+eldray@users.noreply.github.com>
2023-07-25 12:52:45 +0000

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,39 @@
-#!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
-import sys
+import requests
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-<<<<<<< HEAD
-    [print("\t {}".format(title)) for title in completed]
-=======
-    [print("\t {}".format(title)) for title in completed]
->>>>>>> d6a8d0dcf23b28829cd6b9cf5fd4f6415a4cc277
+def get_employee_todo_progress(employee_id):
+    # Fetch employee information from the REST API
+    base_url = 'https://jsonplaceholder.typicode.com'
+    response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = response.json()
+
+    # Fetch TODO list for the employee from the REST API
+    response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = response.json()
+
+    # Get the employee name and TODO list progress
+    employee_name = user_data['name']
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    # Display employee TODO list progress
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+
+    # Display completed task titles
+    for todo in todos_data:
+        if todo['completed']:
+            print("\t", todo['title'])
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_employee_todo_progress(employee_id)
