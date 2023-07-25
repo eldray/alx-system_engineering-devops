734ee7e7d6036180e9a0b271b4c836d79d13e9e6
eldray <83376852+eldray@users.noreply.github.com>
2023-07-25 13:18:22 +0000

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,22 +5,18 @@ import requests as r
 import json
 import sys
 
-def export_to_json(employee_id, todos_data):
-    employee_name = todos_data[0]['name']
-    json_data = {
-        employee_id: [
-            {
-                "task": todo['title'],
-                "completed": todo['completed'],
-                "username": employee_name
-            }
-            for todo in todos_data
-        ]
-    }
+def export_to_json(employee_id, todos_data, employee_name):
+    todo_list = []
+    for todo in todos_data:
+        task_title = todo['title']
+        completed = todo['completed']
+        todo_list.append({"task": task_title, "completed": completed, "username": employee_name})
 
+    output_data = {employee_id: todo_list}
     file_name = f"{employee_id}.json"
-    with open(file_name, mode='w') as json_file:
-        json.dump(json_data, json_file, indent=4)
+
+    with open(file_name, 'w') as json_file:
+        json.dump(output_data, json_file, indent=4)
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
@@ -28,4 +24,5 @@ if __name__ == '__main__':
     usr_data = r.get(url + 'users/{}'.format(employee_id)).json()
     to_do = r.get(url + 'todos', params={'userId': employee_id}).json()
 
-    export_to_json(employee_id, to_do)
+    employee_name = usr_data['name']
+    export_to_json(employee_id, to_do, employee_name)
