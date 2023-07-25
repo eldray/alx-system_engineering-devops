d35d2d5469989752471d882a0e1cc5d55d2662bd
eldray <83376852+eldray@users.noreply.github.com>
2023-07-25 13:22:45 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,8 +4,16 @@ import requests as r
 import csv
 import sys
 
+def get_employee_name(employee_id):
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = r.get(url)
+    if response.status_code == 200:
+        return response.json()['name']
+    else:
+        raise ValueError(f"Could not fetch user information for employee ID {employee_id}")
+
 def export_to_csv(employee_id, todos_data):
-    employee_name = todos_data[0]['name']
+    employee_name = get_employee_name(employee_id)
     file_name = f"{employee_id}.csv"
 
     with open(file_name, mode='w', newline='') as csv_file:
@@ -22,9 +30,23 @@ def export_to_csv(employee_id, todos_data):
             })
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    employee_id = sys.argv[1]
-    usr_data = r.get(url + 'users/{}'.format(employee_id)).json()
-    to_do = r.get(url + 'todos', params={'userId': employee_id}).json()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    params = {'userId': employee_id}
+    response = r.get(url, params=params)
+
+    if response.status_code != 200:
+        print("Error: Could not fetch TODO data for the specified employee ID.")
+        sys.exit(1)
+
+    to_do = response.json()
     export_to_csv(employee_id, to_do)
