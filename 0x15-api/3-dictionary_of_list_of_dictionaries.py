5417bbb227929bdc56ac3fa9533495a2ed6ef439
eldray <83376852+eldray@users.noreply.github.com>
2023-07-25 12:53:02 +0000

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
 """Exports to-do list information of all employees to JSON format."""
+
 import json
 import requests
+from sys import argv
+
+
+def export_all_to_json():
+    if len(argv) != 1:
+        return
+
+    url = "https://jsonplaceholder.typicode.com/users"
+    users_response = requests.get(url)
+    users_data = users_response.json()
+
+    all_tasks = {}
+
+    for user in users_data:
+        user_id = str(user["id"])
+        url = "https://jsonplaceholder.typicode.com/todos?userId=" + user_id
+        todos_response = requests.get(url)
+        todos_data = todos_response.json()
+
+        tasks = []
+        for task in todos_data:
+            task_dict = {
+                "username": user["username"],
+                "task": task["title"],
+                "completed": task["completed"],
+            }
+            tasks.append(task_dict)
+
+        all_tasks[user_id] = tasks
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_tasks, json_file)
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-<<<<<<< HEAD
-            for u in users}, jsonfile)
-=======
-            for u in users}, jsonfile)
->>>>>>> d6a8d0dcf23b28829cd6b9cf5fd4f6415a4cc277
+    export_all_to_json()
