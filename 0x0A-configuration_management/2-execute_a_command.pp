ef2bbada85ba41954f32d37edee484e54d4b265b
Emmanuel Appiah <emk.appiah@gmail.com>
2023-10-25 04:15:53 +0000

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# kill a process
-exec { 'pkill_killmenow':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'],
-}
